a1a9b733c957a85cfc600cff651ba29d98b842b0
HuCourt <HuCourt@users.noreply.github.com>
2021-12-15 03:57:25 +0100

    Add build script

---
+++ buildAddon.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+${SCRIPT_DIR}/.release/release.sh -z -d -r "/mnt/e/SSD_Games/World of Warcraft/_retail_/Interface/Addons" -p 19268
